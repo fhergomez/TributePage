b0c608004a370aec238834dda6dc1c10b72b9635
fhergomez <fhergomez37@gmail.com>
2016-05-15 01:16:03 -0700

    add routes to express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-'use strict'
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.get('/api/tributes', function(req,res){
+  res.send('these are Mexico great tributes');
+});
+
+app.listen(3000, function() {
+  console.log('Your app is running on port 3000!');
+});
