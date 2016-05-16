e88e666c8e116356260f03208588c3898187f76f
fhergomez <fhergomez37@gmail.com>
2016-05-15 22:24:52 -0700

    add favorites.json file

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var favorites = require('../../mock/favorites.json')
+var router = express.Router();
+
+router.get('/tributes', function(req,res){
+  res.json({todos: {}});
+});
+
+//TODO: add POST route to add favorite
+
+//TODO: add PUT route to update existing entries
+
+//TODO: add DELETE route to delete entries
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 app.use('/', express.static('public'));
-
-var router = express.Router();
-
-router.get('/tributes', function(req,res){
-  res.json({todos: {}});
-});
-
-//TODO: add POST route to add favorite
-
-//TODO: add PUT route to update existing entries
-
-//TODO: add DELETE route to delete entries
 
 app.use('/api', router);
 
